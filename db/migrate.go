827780f9bda3ad3b128b987ff7304074e16a479b
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2024-01-26 18:49:25 +0330

    feat: add password field to users table/model

--- db/migrate.go
+++ db/migrate.go
@@ -3,5 +3,5 @@ package db
 import "example.com/golang-crud-gorm/models"
 
 func Migrate() error {
-	return DB().Migrator().AlterColumn(&models.Todo{}, "UserID")
+	return DB().Migrator().AddColumn(&models.User{}, "Password")
 }
